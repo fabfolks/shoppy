512625df3de0df649333e18c0d3b759043962c38
Sumanth Nagadavalli <sumanth.nagadavalli@rackspace.com>
2013-10-26 15:54:03 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shoppy::Application.config.secret_key_base = 'e7fbd942963c85273562aacb54363eb04423a524e975b079532ad1c2a7969f5479c1dc7d5f0583d954e1ff3e45bfcc2c1c009621ab5de81f5b82c18f9285e740'
